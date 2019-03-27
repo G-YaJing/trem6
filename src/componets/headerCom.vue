8e1bfc5c1bcc296e0596b7ddc332c5350c77765d
Your Name <you@example.com>
2019-03-27 09:07:56 +0800

    sjx

---
+++ src/componets/headerCom.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>购物车(2)</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+ header{
+     width: 100%;
+     height: 60px;
+     background: green;
+     color: white;
+     text-align: center;
+     line-height: 60px;
+     /* position: fixed;
+     top: 0;
+     le */
+ }
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import headercom from './components/headerCom.vue'
+import footercom from './components/footerCom.vue'
+
+new Vue({
+    el: '#app',
+    components: { headercom, footercom, listcom }
+})
